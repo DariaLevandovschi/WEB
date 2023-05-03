13b515a93f14113ec4a77fcd92f2eef56f7332a3
DariaLevandovschi <daria.levandovschi@iis.utm.md>
2023-05-03 15:21:09 +0300

    NewProject

--- TedMarket/src/App.jsx
+++ TedMarket/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const handleClick = () => {
+        alert('Sorry, the shop is currently closed. Please have patience 🙂');
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="app">
+            <nav>
+                <h1>Teddy Bear Shop</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <main>
+                <h2>Welcome to our shop!</h2>
+                <p>Best teddy bears in Republic of Moldova.</p>
+                <button onClick={handleClick}>Shop Now</button>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
